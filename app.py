1cdcf02a31aad182be92dd730a7bcace3e924830
toilaluan <toilaluan@github.com>
2023-11-20 11:54:06 +0000

    update temperature, top_p as input, return seeds, strengths

--- app.py
+++ app.py
@@ -1,56 +1,73 @@
 from typing import List
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
 import yaml
+from pydantic.fields import Field
 from utils import model_processing, load_base_model
 from vllm import SamplingParams
-from vllm.entrypoints.openai.protocol import (
-    CompletionResponse, CompletionResponseChoice, UsageInfo
-)
+from vllm.entrypoints.openai.protocol import CompletionResponseChoice, UsageInfo
 from vllm.utils import random_uuid
 import time
 
 CONFIG = yaml.load(open("config.yaml"), yaml.FullLoader)
 
+
+class CompletionResponse(BaseModel):
+    id: str = Field(default_factory=lambda: f"cmpl-{random_uuid()}")
+    object: str = "text_completion"
+    created: int = Field(default_factory=lambda: int(time.time()))
+    model: str
+    choices: List[CompletionResponseChoice]
+    usage: UsageInfo
+    seeds: List[int]
+    strengths: List[float]
+    prompts: List[str]
+
+
 # Define the data model
 class DataModel(BaseModel):
     seeds: List[int]
-    strengths: List[int]
+    strengths: List[float]
     prompts: List[str]
     max_length: int
+    temperature: float
+    top_p: float
+
 
 # Create a FastAPI instance
 app = FastAPI()
 
-BASE_MODEL, TOKENIZER = load_base_model(CONFIG['base_model_name'])
+BASE_MODEL, TOKENIZER = load_base_model(CONFIG["base_model_name"])
 
-# Define the POST endpoint
+
 @app.post("/generate")
 async def perturb_generation(data: DataModel):
     request_id = f"cmpl-{random_uuid()}"
     created_time = int(time.monotonic())
-    model = model_processing(base_model = BASE_MODEL, tokenizer=TOKENIZER, seeds=data.seeds, strengths=data.strengths)
+    model = model_processing(
+        base_model=BASE_MODEL,
+        tokenizer=TOKENIZER,
+        seeds=data.seeds,
+        strengths=data.strengths,
+    )
     prompts = data.prompts
-    # change sampling_params if needed
-    sampling_params = SamplingParams(temperature=0.8, top_p=0.95, max_tokens=data.max_length)
+    sampling_params = SamplingParams(
+        temperature=data.temperature, top_p=data.top_p, max_tokens=data.max_length
+    )
     batch_outputs = model.generate(prompts, sampling_params)
     responses = []
     for outputs in batch_outputs:
         choices = []
         for output in outputs.outputs:
-            print(output)
-            logprobs=None
             choice_data = CompletionResponseChoice(
                 index=output.index,
                 text=output.text,
-                logprobs=logprobs,
                 finish_reason=output.finish_reason,
             )
             choices.append(choice_data)
 
         num_prompt_tokens = len(outputs.prompt_token_ids)
-        num_generated_tokens = sum(
-            len(output.token_ids) for output in outputs.outputs)
+        num_generated_tokens = sum(len(output.token_ids) for output in outputs.outputs)
         usage = UsageInfo(
             prompt_tokens=num_prompt_tokens,
             completion_tokens=num_generated_tokens,
@@ -59,13 +76,19 @@ async def perturb_generation(data: DataModel):
         response = CompletionResponse(
             id=request_id,
             created=created_time,
-            model=f"{CONFIG['base_model_name']}-seed-{'-'.join(map(str,data.seeds))}-strength-{'-'.join(map(str,data.strengths))}",
+            model=CONFIG["base_model_name"],
             choices=choices,
             usage=usage,
+            seeds=data.seeds,
+            strengths=data.strengths,
+            prompts=data.prompts,
         )
         responses.append(response)
     return responses
+
+
 # In the main block, you can run the app using Uvicorn
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
